84bb3b368081b994117bf25cd62ee248b9d4687b
SAMER MAJID AL-BAHABSA <62020378+Samer7777@users.noreply.github.com>
2021-07-05 18:12:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+/*navbar */
+const humberger =document.getElementById('humburger')
+const navul =document.getElementById('navbar')
+
+humberger.addEventListener('click', ()=>{
+  navul.classList.toggle('show')
+});
+
